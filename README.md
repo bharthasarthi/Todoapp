ece58d8a42167e139172438b58aa98951c7ff2e9
bharthasarathi <bharthassr1320@gmail.com>
2024-02-29 14:36:34 +0530

    todoapp

---
+++ README.md
@@ -0,0 +1,7 @@
+# todoapp
+
+**## Getting Started**
+
+This project is a starting point for a Flutter application.
+
+Todoapp used to your daily task easily noted and that task in finshed in easily delete the task.
---
+++ lib/main.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hydrated_bloc/hydrated_bloc.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:todoapp/Home.dart';
+import 'package:todoapp/bloc/todo_bloc.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  HydratedBloc.storage = await HydratedStorage.build(
+    storageDirectory: await getTemporaryDirectory(),
+  );
+
+  runApp(const MainApp());
+}
+
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Todo App',
+      theme: ThemeData(
+        colorScheme: const ColorScheme.light(
+            background: Colors.white,
+            onBackground: Colors.black,
+            primary: Colors.lightBlue,
+            onPrimary: Colors.black,
+            secondary: Colors.lightGreen,
+            onSecondary: Colors.white),
+      ),
+      home: BlocProvider<TodoBloc>(
+        create: (context) => TodoBloc()..add(TodoStarted()),
+        child: const HomeScreen(),
+      ),
+    );
+  }
+}
